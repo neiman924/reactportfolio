138c7cbd9d7e61fc8dfe6e468cbc9131328a5e99
Hossein Neiman <neiman924@gmail.com>
2022-09-08 18:00:46 -0700

    update client package.json

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,21 +1,21 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-
-// module.exports = mongoose.connection;
-
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://googlebooks:googlebooks@cluster0.z6ecx7d.mongodb.net/?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
 module.exports = mongoose.connection;
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect("mongodb+srv://googlebooks:googlebooks@cluster0.z6ecx7d.mongodb.net/?retryWrites=true&w=majority", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+// module.exports = mongoose.connection;
--- server/config/dist/connection.dev.js
+++ server/config/dist/connection.dev.js
@@ -1,20 +1,16 @@
 "use strict";
 
-// const mongoose = require('mongoose');
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-// module.exports = mongoose.connection;
 var mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://googlebooks:googlebooks@cluster0.z6ecx7d.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
-module.exports = mongoose.connection;
+module.exports = mongoose.connection; // const mongoose = require('mongoose');
+// mongoose.connect("mongodb+srv://googlebooks:googlebooks@cluster0.z6ecx7d.mongodb.net/?retryWrites=true&w=majority", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+// module.exports = mongoose.connection;
