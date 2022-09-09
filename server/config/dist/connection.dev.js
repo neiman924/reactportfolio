85294fa28f67b5d34eca63ca8ad5a228e3385bf1
Hossein Neiman <neiman924@gmail.com>
2022-09-08 18:26:34 -0700

    update client package.json

--- server/config/dist/connection.dev.js
+++ server/config/dist/connection.dev.js
@@ -1,16 +1,18 @@
 "use strict";
 
+// const mongoose = require('mongoose');
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
+// module.exports = mongoose.connection;
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio', {
+mongoose.connect("mongodb+srv://googlebooks:googlebooks@cluster0.z6ecx7d.mongodb.net/portfolio?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-module.exports = mongoose.connection; // const mongoose = require('mongoose');
-// mongoose.connect("mongodb+srv://googlebooks:googlebooks@cluster0.z6ecx7d.mongodb.net/?retryWrites=true&w=majority", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
-// module.exports = mongoose.connection;
+module.exports = mongoose.connection;
